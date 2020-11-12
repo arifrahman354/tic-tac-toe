860c383128d63e7b58c31beb11f1f48a099ff8df
arifrahman354 <74341282+arifrahman354@users.noreply.github.com>
2020-11-12 15:29:16 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import TicTacToe from "./TicTacToe";
+import "papercss/dist/paper.min.css";
+
+function App() {
+  return (
+    <>
+      <Main>
+        <TicTacToe />
+      </Main>
+      <Footer>
+        <FooterInner>
+          TicTacToe 
+          Arif Rahman Muttaqin (1119101807)
+          <a href="https://github.com/arifrahman354"> Github</a>
+        </FooterInner>
+      </Footer>
+    </>
+  );
+}
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 auto;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const FooterInner = styled.div`
+  padding: 16px 0;
+  color: Red;
+`;
+export default App;
